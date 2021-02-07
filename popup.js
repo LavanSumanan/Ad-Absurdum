fa0b6daf5505fe86e33694a59e8830928702c06f
LavanSumanan <lavan.sumanan@gmail.com>
2021-02-06 21:29:53 -0500

    Fixed popup.js

--- popup.js
+++ popup.js
@@ -1,15 +1,16 @@
-document.getElementsByClassName("theme").addEventListener('click', function (event) {
-    var elems = document.getElementsByClassName("theme");
-    for (var j = 0; j < elems.length; j++) {
-        var color = window.getComputedStyle(this, null).getPropertyValue("background-color");
-        if (color === "rgb(253, 107, 182)") {
-            for (var i = 0; i < elems.length; i++) {
-                elems[i].style.backgroundColor = "rgb(253, 107, 182)";
-            }
-            this.style.backgroundColor = "rgb(0, 169, 254)";
-        }
-        else {
-            this.style.backgroundColor = "rgb(253, 107, 182)";
-        }
+var elems = document.getElementsByClassName("theme");
+for (var i = 0; i < elems.length; i++) {
+  elems[i].onclick = function() {
+    var color = window.getComputedStyle(this, null)
+                .getPropertyValue("background-color");
+    if (color === "rgb(253, 107, 182)") {
+      for (var i = 0; i < elems.length; i++) {
+        elems[i].style.backgroundColor = "rgb(253, 107, 182)";
+      }
+      this.style.backgroundColor = "rgb(0, 169, 254)";
     }
-});
+    else {
+      this.style.backgroundColor = "rgb(253, 107, 182)";
+    }
+  };
+}
