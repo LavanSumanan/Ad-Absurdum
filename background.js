1ae9f91c9c681e569cb2597daa31b164814f5c1e
LavanSumanan <lavan.sumanan@gmail.com>
2021-02-06 23:55:12 -0500

    Picks random link from theme and redirects ads to that image

--- background.js
+++ background.js
@@ -4,21 +4,48 @@ Puppers
 2. "https://i.ibb.co/1frMpg8/p2.png"
 3. "https://i.ibb.co/M6Tznbv/p3.png"
 Kitties
-1. ""
-2. ""
-3. ""
+1. "https://i.ibb.co/4RL5ymT/k1.png"
+2. "https://i.ibb.co/ZN12YVy/k2.png"
+3. "https://i.ibb.co/xSN8zsf/k3.png"
 Inspiration
-1. ""
-2. ""
-3. ""
+1. "https://i.ibb.co/wgbbMms/in1.png"
+2. "https://i.ibb.co/VTqVq36/in2.png"
+3. "https://i.ibb.co/VLntjfr/in3.png"
 Imagines
-1. ""
-2. ""
-3. ""
+1. "https://i.ibb.co/K5gFpXs/im1.png"
+2. "https://i.ibb.co/xJNYBMd/im2.png"
+3. "https://i.ibb.co/pwr9Ks6/im3.png"
+*/
+
+var pupper = ["https://i.ibb.co/nqGQTRx/p1.png", "https://i.ibb.co/1frMpg8/p2.png", "https://i.ibb.co/M6Tznbv/p3.png"];
+var kitties = ["https://i.ibb.co/4RL5ymT/k1.png", "https://i.ibb.co/ZN12YVy/k2.png", "https://i.ibb.co/xSN8zsf/k3.png"];
+var inspiration = ["https://i.ibb.co/wgbbMms/in1.png", "https://i.ibb.co/VTqVq36/in2.png", "https://i.ibb.co/VLntjfr/in3.png"];
+var imagines = ["https://i.ibb.co/K5gFpXs/im1.png", "https://i.ibb.co/xJNYBMd/im2.png", "https://i.ibb.co/pwr9Ks6/im3.png"];
+
+function pickImage(theme) {
+    var ran = Math.floor(Math.random()*3);
+    if (theme === "pupper") {
+        return pupper[ran];
+    } else if (theme === "kitties") {
+        return kitties[ran];
+    } else if (theme === "inspiration") {
+        return inspiration[ran];
+    } else if (theme === "imagines") {
+        return imagines[ran];
+    }
+}
+
+/*
+function updateSize(imgURL) {
+    var img = document.createElement("img");
+    img.src=imgURL;
+}
 */
 
 chrome.webRequest.onBeforeRequest.addListener(
-    function (details) { return { redirectUrl: "https://i.ibb.co/KsG9L3p/pupper-1-2.png" }; },
+    function (details) {
+        return { redirectUrl: pickImage(CHANGE THIS) };
+    },
     { urls: ["*://*.doubleclick.net/*"] },
     ["blocking"]
 );
