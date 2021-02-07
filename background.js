182251440b51eb10652244367de06c3387562f81
LavanSumanan <lavan.sumanan@gmail.com>
2021-02-06 21:49:22 -0500

    Added pupper

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
+
 chrome.webRequest.onBeforeRequest.addListener(
-    function (details) { return { redirectUrl: "https://photos.app.goo.gl/PUQiHYuTvgoVetos7" }; },
+    function (details) { return { redirectUrl: "https://www.alimentarium.org/en/system/files/thumbnails/image/AL027-01_pomme_de_terre_0.jpg" }; },
     { urls: ["*://*.doubleclick.net/*"] },
     ["blocking"]
 );
