cef81a1200cb70fb5d5421366f0fac84d1656d35
LavanSumanan <lavan.sumanan@gmail.com>
2021-02-08 17:15:00 -0500

    Attempted to change size of images to fit memes

--- background.js
+++ background.js
@@ -18,22 +18,40 @@ Imagines
 */
 
 // Arrays of images per theme
-var pupperArray = ["https://i.ibb.co/L8fMRN9/p1.png", "https://i.ibb.co/MhK1sG0/p2.png", "https://i.ibb.co/WnzJPKJ/p3.png"];
-var kittiesArray = ["https://i.ibb.co/P1h1NJX/k1.png", "https://i.ibb.co/4FyQDr5/k2.png","https://i.ibb.co/tLYRJN1/k3.png"];
-var inspirationArray = ["https://i.ibb.co/Q6k9kVP/in1.png", "https://i.ibb.co/6FgHx4X/in2.png", "https://i.ibb.co/jMN2MXm/in3.png"];
-var imaginesarray = ["https://i.ibb.co/K5gFpXs/im1.png", "https://i.ibb.co/xJNYBMd/im2.png", "https://i.ibb.co/pwr9Ks6/im3.png"];
+var images =
+    [
+        [
+            ["https://i.ibb.co/L8fMRN9/p1.png", 400, 267],
+            ["https://i.ibb.co/MhK1sG0/p2.png", 400, 267],
+            ["https://i.ibb.co/WnzJPKJ/p3.png", 400, 267]
+        ],
+        [
+            ["https://i.ibb.co/P1h1NJX/k1.png", 400, 300],
+            ["https://i.ibb.co/4FyQDr5/k2.png", 400, 300],
+            ["https://i.ibb.co/tLYRJN1/k3.png", 400, 300]
+        ],
+        [
+            ["https://i.ibb.co/Q6k9kVP/in1.png", 400, 300],
+            ["https://i.ibb.co/6FgHx4X/in2.png", 400, 300],
+            ["https://i.ibb.co/jMN2MXm/in3.png", 400, 300]
+        ],
+        [
+            ["https://i.ibb.co/K5gFpXs/im1.png", 100, 75],
+            ["https://i.ibb.co/xJNYBMd/im2.png", 400, 960],
+            ["https://i.ibb.co/pwr9Ks6/im3.png", 400, 176]
+        ]
+    ];
 
 // function to pick a random image from a theme (given a theme)
 function pickImage(theme) {
-    var ran = Math.floor(Math.random() * 3);
-    if (theme === "pupper") {
-        return pupperArray[ran];
-    } else if (theme === "kitties") {
-        return kittiesArray[ran];
-    } else if (theme === "inspiration") {
-        return inspirationArray[ran];
-    } else if (theme === "imagines") {
-        return imaginesarray[ran];
+    if (theme == "pupper") {
+        return 0;
+    } else if (theme == "kitties") {
+        return 1;
+    } else if (theme == "inspiration") {
+        return 2;
+    } else if (theme == "imagines") {
+        return 3;
     }
 }
 
@@ -42,8 +60,20 @@ var currentTheme;
 // Fires when a server requests to display. Calls "pickImage(theme)" to find a URL for an image in the given theme "currentTheme" 
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
-        var URL = pickImage(currentTheme);
+        var ran = Math.floor(Math.random() * 3);
+        var URL = images[pickImage(currentTheme)][ran][0];
+        var width = images[pickImage(currentTheme)][ran][1];
+        var length = images[pickImage(currentTheme)][ran][2]
         if (URL != "none") {
+            // Send message to content.js to resize ad containers to fit meme.            
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    action: "resize",
+                    url: URL,
+                    width: width,
+                    length: length
+                });
+            });
             return { redirectUrl: URL };
         }
     },
@@ -64,7 +94,8 @@ for (var i = 0; i < elems.length; i++) {
             }
             this.style.backgroundColor = "rgb(0, 169, 254)";
             // Set currentTheme to the chosen button's id
-            currentTheme = this.id;
+            currentTheme = this.id+"";
+            alert(currentTheme);
         }
         else {
             //  Changes colour of button back to pink
@@ -75,9 +106,6 @@ for (var i = 0; i < elems.length; i++) {
 
         // Reloads current tab
         chrome.tabs.reload();
-
-        // Sends message to content.js to reload page
-        chrome.tabs.sendMessage({action: "reload"});
 
     };
 
--- content.js
+++ content.js
@@ -1,8 +1,14 @@
-// When it receives a message from background.js with the request "reload", reloads the website.
+// When it receives a message from background.js with the request "resize", it finds all image elements with an src of the meme,
+// and then resizes the image element to fit the ad.
 chrome.runtime.onMessage.addListener(
     function (message) {
-        if (message.action == "reload") {
-            window.location.reload();
-            return false;
+        if (message.action == "resize") {
+            var elements = document.getElementsByTagName('img');
+            for (var i=0; i < elements.length; i++) {
+                if (elements[i].src == message.url) {
+                    elements[i].width = message.width;
+                    elements[i].height = message.height;
+                }
+            }
         }
     });
